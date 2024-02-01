9c8164af77680fcc3557a9667c133aa25749bd09
tini-yu <i.novoselova.sas@gmail.com>
2024-02-01 13:30:48 +0300

    Removed unnecessary files; Added .env and connected to main; Fixed .eslint false-highlight issue.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  //:number проверяет, что port это number ещё на этапе написания кода
+  const port: number = parseInt(process.env.PORT); //parseInt преобразует строку в целочисленное число
+  console.log(port);
+  const server = process.env.SERVER; // take port and server info from .env
+  await app.listen(port, server);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
